38e5f35b7951f20360614e48f3030d878c108d58
ibrahimdal <ibrahimdal999@gmail.com>
2020-02-09 23:06:04 +0300

    completed.

---
+++ SampleForApiDockerize/SampleForApiDockerize/Controllers/FooBarController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleForApiDockerize.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FooBarController : ControllerBase
+    {
+        public FooBarController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("Read")]
+        public async Task<IActionResult> Read()
+        {
+            return await Task.FromResult(Ok(1));
+        }
+    }
+}
